a11f611e2efd2e15598f50fec680e7756e2375dd
takeke <takeke0404@yahoo.co.jp>
2020-09-20 01:21:42 +0900

    コメントの取得

---
+++ get_video/get_comment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+while read a b
+do
+    video_id=${a##*=}
+    filename_list=()
+    for filename in "comments/*"; do
+        filename_list+="$filename"
+    done
+    if [[ $(printf '%s\n' "${filename_list[@]}" | grep -qx "comments/$b.json"; echo -n ${?} ) -eq 0 ]]; then
+        :
+    else
+        echo "download comment"
+        python -c "import get_comment; get_comment.get_comment_json('$video_id','comments/$b.json')"
+    fi
+done < ./name_list.txt
