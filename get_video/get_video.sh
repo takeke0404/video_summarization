d6ebc88575bdae6e6852b6eeaaa56bdf703ad7c0
takeke <takeke0404@yahoo.co.jp>
2020-09-20 01:55:39 +0900

    wavで保存 listの重複を考慮

--- get_video/get_video.sh
+++ get_video/get_video.sh
@@ -5,9 +5,14 @@ for filename in $videos; do
     videolist+="$filename"
 done
 name_list=()
+prv_video_list=()
 while read a b
 do
-    youtube-dl -x --extract-audio --audio-quality 0 --audio-format mp3 --no-post-overwrites $b -o "videos/%(title)s.%(ext)s"
+    if [[ $(printf '%s\n' "${prv_video_list[@]}" | grep -qx "$b"; echo -n ${?} ) -eq 0 ]]; then
+        continue
+    fi
+    prv_video_list+="$b"
+    youtube-dl -x --extract-audio --audio-quality 0 --audio-format wav $b -o "videos/%(title)s.%(ext)s"
     name_list+="$b "
     name=""
     for filename in $videos; do
@@ -22,6 +27,6 @@ do
     name=${name%.*}
     name_list+="$name"
     name_list+=$'\n'
-    youtube-dl -x --extract-audio --audio-quality 0 --audio-format mp3 --no-post-overwrites $a -o "crips/$name.%(ext)s"
+    youtube-dl -x --extract-audio --audio-quality 0 --audio-format wav $a -o "crips/$name.%(ext)s"
 done < ./list.txt
 echo $name_list > name_list.txt
