ba0e84cb3a2266110ef5cc7b6d2579f5a8186145
takeke <takeke0404@yahoo.co.jp>
2020-09-19 18:59:05 +0900

    同じ動画はダウンロードしないように変更

--- get_video/get_video.sh
+++ get_video/get_video.sh
@@ -4,9 +4,9 @@ videolist=()
 for filename in $videos; do
     videolist+="$filename"
 done
-while read a b c
+while read a b
 do
-    youtube-dl -x --extract-audio --audio-format mp3 $b -o "videos/%(title)s.%(ext)s"
+    youtube-dl -x --extract-audio --audio-quality 0 --audio-format mp3 --no-post-overwrites $b -o "videos/%(title)s.%(ext)s"
     name=""
     for filename in $videos; do
         if [[ $(printf '%s\n' "${videolist[@]}" | grep -qx "$filename"; echo -n ${?} ) -eq 0 ]]; then
@@ -18,5 +18,5 @@ do
     videolist+="$name"
     name=${name##*/}
     name=${name%.*}
-    youtube-dl -x --extract-audio --audio-format mp3 $a -o "crips/$name.%(ext)s"
+    youtube-dl -x --extract-audio --audio-quality 0 --audio-format mp3 --no-post-overwrites $a -o "crips/$name.%(ext)s"
 done < ./list.txt
