3a8b065ed8677ec61def10e5beda7838a3fd858b
takeke <takeke0404@yahoo.co.jp>
2020-09-19 19:45:10 +0900

    配信の名前をファイルに保存

--- get_video/get_video.sh
+++ get_video/get_video.sh
@@ -4,9 +4,11 @@ videolist=()
 for filename in $videos; do
     videolist+="$filename"
 done
+name_list=()
 while read a b
 do
     youtube-dl -x --extract-audio --audio-quality 0 --audio-format mp3 --no-post-overwrites $b -o "videos/%(title)s.%(ext)s"
+    name_list+="$b "
     name=""
     for filename in $videos; do
         if [[ $(printf '%s\n' "${videolist[@]}" | grep -qx "$filename"; echo -n ${?} ) -eq 0 ]]; then
@@ -18,5 +20,8 @@ do
     videolist+="$name"
     name=${name##*/}
     name=${name%.*}
+    name_list+="$name"
+    name_list+=$'\n'
     youtube-dl -x --extract-audio --audio-quality 0 --audio-format mp3 --no-post-overwrites $a -o "crips/$name.%(ext)s"
 done < ./list.txt
+echo $name_list > name_list.txt
