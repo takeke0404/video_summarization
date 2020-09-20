09385c2fdbc74a28172cc6168f77ffc8d80aeeb8
takeke <takeke0404@yahoo.co.jp>
2020-09-20 20:18:14 +0900

    name_list削除 コメント取得エラー時にファイルを生成しないように変更

--- get_video/get_comment.py
+++ get_video/get_comment.py
@@ -3,5 +3,7 @@ import youtube_chat_replay_crawler as yc
 import json
 def get_comment_json(video_id,output_filename):
  s=yc.get_chat_replay_data(video_id)
+ if s==[]:
+     return
  with open(output_filename, mode='w') as f:
       f.write(json.dumps(s,ensure_ascii=False))
