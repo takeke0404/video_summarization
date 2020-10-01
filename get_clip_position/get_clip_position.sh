bb33c6e32b84a1d38a46ec0f08cea278e976eea6
takeke <takeke0404@yahoo.co.jp>
2020-10-02 01:00:24 +0900

    permission

--- get_clip_position/get_clip_position.sh
+++ get_clip_position/get_clip_position.sh
@@ -13,7 +13,10 @@ do
         echo "output/$b.txt exists"
     else
         echo "get clip_position $b"
-        python -B -u -c "import get_clip_position; import sys; get_clip_position.get_clip_position(sys.argv[1],sys.argv[2])" "../get_video/videos/$b.wav" "../get_video/crips/$b.wav" |& tee output/$b.txt
+        python -B -u -c "import get_clip_position; import sys; get_clip_position.get_clip_position(sys.argv[1],sys.argv[2])" "../get_video/videos/$b.wav" "../get_video/clips/$b.wav" |& tee output/$b.txt
+        chmod 777 "output/$b.txt"
+        chmod 777 "positions/$b.txt"
+        chmod 777 "clips/$b.wav"
     fi
 done < ../get_video/name_list.txt
 echo "get_clip_position is done"
